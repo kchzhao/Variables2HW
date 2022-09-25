42fef2c805e83af97f1c63cd57a3497ab4b18c34
kchzhao <kamila.chzhao@gmail.com>
2022-09-25 01:53:41 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        //Exercise 1
+        byte age = 22;
+        int year = 2022;
+        short birthtYear = 1999;
+        long salary = 1999999;
+        float experience = 2.5f;
+        double height = 165.5;
+        System.out.println("Значение переменной age с типом byte равно " + age);
+        System.out.println("Значение переменной year с типом int равно " + year);
+        System.out.println("Значение переменной birthYear с типом short равно " + birthtYear);
+        System.out.println("Значение переменной salary с типом long равно " + salary);
+        System.out.println("Значение переменной experience с типом float равно " + experience);
+        System.out.println("Значение переменной height с типом double равно " + height);
+
+        //Excercise 2
+    }
+}
